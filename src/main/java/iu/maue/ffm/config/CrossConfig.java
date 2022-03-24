9f9a2486d0443efb2b02560e17e19c7be16ad5ec
lindseyxcdh <822466858@qq.com>
2022-03-24 18:10:02 +0800

    Add CrossConfig

---
+++ src/main/java/iu/maue/ffm/config/CrossConfig.java
@@ -0,0 +1,27 @@
+package iu.maue.ffm.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * 跨域
+ */
+@Configuration
+public class CrossConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        final CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOriginPattern("*");
+        config.setAllowCredentials(true);
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        config.addExposedHeader("*");
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
